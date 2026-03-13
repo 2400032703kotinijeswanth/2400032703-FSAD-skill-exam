dfa99a7b603f8ccd78c038fac0154775d0560762
2400032703kotinijeswanth <2400032703@kluniversity.in>
2026-03-13 09:18:46 +0530

    Add files via upload

---
+++ exam/exam/src/main/java/com/klef/fsad/exam/ClientDemo.java
@@ -0,0 +1,31 @@
+package com.klef.fsad.exam;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+public class ClientDemo 
+{
+ public static void main(String[] args) 
+ {
+  SessionFactory sf = new Configuration().configure().buildSessionFactory();
+  Session session = sf.openSession();
+  Transaction tx = session.beginTransaction();
+
+  Supplier s = new Supplier();
+  s.setName("ABC");
+  s.setDescription("Electronics");
+  s.setDate("2026-03-12");
+  s.setStatus("Active");
+
+  session.save(s);
+
+  Supplier s1 = session.get(Supplier.class,1);
+  s1.setName("Updated");
+  s1.setStatus("Inactive");
+
+  session.update(s1);
+
+  tx.commit();
+  session.close();
+ }
+}
