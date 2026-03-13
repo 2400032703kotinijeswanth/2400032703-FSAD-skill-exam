0c18a74fd10b70017925c13bd8ac61303c032f36
2400032703kotinijeswanth <2400032703@kluniversity.in>
2026-03-13 09:22:49 +0530

    Intial commit

--- exam/exam/src/main/java/com/klef/fsad/exam/ClientDemo.java
+++ exam/exam/src/main/java/com/klef/fsad/exam/ClientDemo.java
@@ -3,6 +3,8 @@ package com.klef.fsad.exam;
 import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 
+import jakarta.websocket.Session;
+
 public class ClientDemo 
 {
  public static void main(String[] args) 
